898b6210a8ef9ac0a552e91c8b4637c3390d496f
Benyamain <yacoobbenyamain@gmail.com>
2023-02-14 21:34:44 -0500

    Updating "Degree" to be an EditText

--- app/src/main/java/com/example/biography/MainActivity.kt
+++ app/src/main/java/com/example/biography/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.biography
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.RadioButton
 import android.widget.Spinner
@@ -11,7 +13,7 @@ import com.example.biography.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var radioBtnText: String = ""
+    private var selectedRadioButtonsList = mutableListOf<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,19 +26,13 @@ class MainActivity : AppCompatActivity() {
         val spinner = findViewById<Spinner>(binding.spinnerMajor.id)
         spinner.adapter = adapter
 
-        binding.radioGroup.setOnCheckedChangeListener { _, checkedId ->
-            if (checkedId != -1) {
-                radioBtnText = findViewById<RadioButton>(checkedId).text.toString()
-                intent.putExtra("SELECTED_DEGREE", radioBtnText)
-            }
-        }
-
         binding.createButton.setOnClickListener {
             val intent = Intent(this, BiographyOutputActivity::class.java)
             intent.putExtra("FIRST_NAME", binding.fName.text.toString())
             intent.putExtra("LAST_NAME", binding.lName.text.toString())
             intent.putExtra("SCHOOL", binding.editTextSchool.text.toString())
             intent.putExtra("GRADUATION_YEAR", binding.editTextGraduationYear.text.toString())
+            intent.putExtra("SELECTED_DEGREE", binding.editTextDegree.text.toString())
             intent.putExtra("FAVORITE_ACTIVITIES", binding.editTextFavoriteActivities.text.toString())
             intent.putExtra("SELECTED_MAJOR", binding.spinnerMajor.selectedItem.toString())
             startActivity(intent)
